d4e542ad43703c066255475400c475d343794c36
Molnár Attila <molnar.attila@szlgbp.hu>
2025-03-06 09:04:35 +0100

    rendeles is van

--- app_krisz_javit/admin.py
+++ app_krisz_javit/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Termek
+from .models import Termek, Rendeles
 
 # Register your models here.
 
 admin.site.register(Termek)
+admin.site.register(Rendeles)
--- app_krisz_javit/models.py
+++ app_krisz_javit/models.py
@@ -15,3 +15,18 @@ class Termek(models.Model):
     def __str__(self):
         return f'{self.nev} -- {self.ar} Ft'
 
+
+class Rendeles(models.Model):
+
+    sorszam = models.IntegerField()
+    termek = models.ForeignKey(Termek, on_delete=models.CASCADE)
+    kartyae = models.BooleanField()
+    rendeles_ideje = models.DateTimeField()
+    elkeszult = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Rendelés"
+        verbose_name_plural = "Rendelések"
+
+    def __str__(self):
+        return f'{self.sorszam}: {self.termek} ({self.rendeles_ideje})' + ('✔' if self.elkeszult else '❌')
